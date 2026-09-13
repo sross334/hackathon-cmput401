89ee9c7d3814869f7e65b6b5894a1c9457c5b6c9
Jonathan Pu <jonathan.puym@gmail.com>
2026-09-13 01:16:07 -0600

    updated apps.py

--- Job-Tracker/backend/applications/apps.py
+++ Job-Tracker/backend/applications/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class ApplicationsConfig(AppConfig):
-    name = 'applications'
+    default_auto_field = "django.db.models.AutoField"
+    name = "applications"
